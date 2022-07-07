1619b683418d8442506e8f394954f74e5c8c1caf
greeng00se <thegreengoose7@gmail.com>
2022-07-07 14:38:02 +0900

    feat: Windi CSS 설정 추가

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,11 @@
 import { defineNuxtConfig } from "nuxt";
 
-const lifecycle = process.env.npm_lifecycle_event;
-
 export default defineNuxtConfig({
   css: ["~/assets/scss/index.scss"],
   build: {
-    transpile: lifecycle === "build" ? ["element-plus"] : [],
+    transpile: ["element-plus"],
   },
+  buildModules: [
+    'nuxt-windicss',
+  ],
 });
--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
     <el-button type="success">Success</el-button>
+    <h1 class="text-orange-600 font-mono">Hello Windy CSS</h1>
   </div>
 </template>
 
