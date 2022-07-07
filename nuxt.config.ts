c92f05c57df41e1ba54c6960e7433ac2be866f8e
greeng00se <thegreengoose7@gmail.com>
2022-07-07 15:09:43 +0900

    feat: iconify 사용
    - build시 오류 수정

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,11 @@
 import { defineNuxtConfig } from "nuxt";
 
+const lifecycle = process.env.npm_lifecycle_event;
+
 export default defineNuxtConfig({
   css: ["~/assets/scss/index.scss"],
   build: {
-    transpile: ["element-plus"],
+    transpile: lifecycle === "build" ? ["element-plus"] : [],
   },
   buildModules: [
     'nuxt-windicss',
--- pages/index.vue
+++ pages/index.vue
@@ -2,9 +2,13 @@
   <div>
     <el-button type="success">Success</el-button>
     <h1 class="text-orange-600 font-mono">Hello Windy CSS</h1>
+    <Icon icon="carbon:align-horizontal-center" />
   </div>
 </template>
 
-<script setup>
-import { ElButton } from "element-plus";
+<script setup lang="ts">
+import { Icon } from "@iconify/vue";
+import { ElButton, ElInput } from "element-plus";
 </script>
+
+<style scoped></style>
