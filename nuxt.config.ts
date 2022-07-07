8ee33845ce4adee32cdaffc1bf59548ca478bbf5
greeng00se <thegreengoose7@gmail.com>
2022-07-07 13:57:50 +0900

    feat: Element Plus 설정 추가

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,10 @@
-import { defineNuxtConfig } from 'nuxt'
+import { defineNuxtConfig } from "nuxt";
 
-// https://v3.nuxtjs.org/api/configuration/nuxt.config
+const lifecycle = process.env.npm_lifecycle_event;
+
 export default defineNuxtConfig({
-
-})
+  css: ["~/assets/scss/index.scss"],
+  build: {
+    transpile: lifecycle === "build" ? ["element-plus"] : [],
+  },
+});
---
+++ pages/index.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    <el-button type="success">Success</el-button>
+  </div>
+</template>
+
+<script setup>
+import { ElButton } from "element-plus";
+</script>
